393368c3e11a62be76b7fb668475de5c7224a0c5
GuguNene <gugunene63@gmail.com>
2023-02-20 17:32:15 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,14 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- * puzzle, followed by new line." 
- * return 0.
+ * puzzle, followed by new line.
+ * Return: Always 0.
  */
 
 int main(void) 
 
 {
-	puts("\"Programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle");
 
-		return(0);
+		return (0);
 }
